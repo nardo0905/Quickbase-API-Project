be8f3968a5b496eff2be722d8e67998ef24ea2fa
Doncho <nardo0905@outlook.com>
2024-06-03 22:05:45 +0300

    Implement connection between github and freshdesk

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,8 @@ package org.example;
 import com.google.gson.Gson;
 import org.example.api.FreshdeskAPI;
 import org.example.api.GithubAPI;
+import org.example.model.FreshdeskContact;
+import org.example.system.GithubFreshdeskConnection;
 
 import java.net.http.HttpClient;
 
@@ -10,15 +12,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        HttpClient client = HttpClient.newHttpClient();
-        Gson gson = new Gson();
-        GithubAPI githubAPI = new GithubAPI(client, gson, "nardo0905");
         try {
-            System.out.println(githubAPI.getUserInfo());
-            FreshdeskAPI freshdeskAPI = new FreshdeskAPI(client, gson, "nardo0905", githubAPI.getUserInfo());
-            freshdeskAPI.createOrUpdateContact();
+            GithubFreshdeskConnection connection = new GithubFreshdeskConnection("nardo0905",
+                    "example");
+            FreshdeskContact contact = connection.connect();
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            System.out.println("Failed to connect to Github and Freshdesk: " + e.getMessage());
         }
     }
 }
--- src/main/java/org/example/system/GithubFreshdeskConnection.java
+++ src/main/java/org/example/system/GithubFreshdeskConnection.java
@@ -1,5 +1,32 @@
 package org.example.system;
 
-public class GithubFreshdeskConnection {
+import com.google.gson.Gson;
+import org.example.api.FreshdeskAPI;
+import org.example.api.GithubAPI;
+import org.example.model.FreshdeskContact;
 
+import java.net.http.HttpClient;
+
+public class GithubFreshdeskConnection {
+    public static final HttpClient client = HttpClient.newHttpClient();
+    public static final Gson gson = new Gson();
+
+    public final String githubUsername;
+    public final String freshdeskSubdomain;
+
+    public GithubFreshdeskConnection(String githubUsername, String freshdeskSubdomain) {
+        this.githubUsername = githubUsername;
+        this.freshdeskSubdomain = freshdeskSubdomain;
+    }
+
+    public FreshdeskContact connect() throws Exception {
+        GithubAPI githubAPI = new GithubAPI(client, gson, githubUsername);
+        System.out.println(githubAPI.getUserInfo());
+        try {
+            FreshdeskAPI freshdeskAPI = new FreshdeskAPI(client, gson, freshdeskSubdomain, githubAPI.getUserInfo());
+            return freshdeskAPI.createOrUpdateContact();
+        } catch (Exception e) {
+            throw new Exception("Failed to connect to Github: " + e.getMessage());
+        }
+    }
 }
